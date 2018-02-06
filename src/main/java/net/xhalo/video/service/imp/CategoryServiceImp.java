0adc7c44ccb6e9004069a40398aa0b000d0b5ff5
Yosun <602704853@qq.com>
2018-02-06 16:29:13 +0800

    搭建Redis缓存，完成FFmpeg工具

--- src/main/java/net/xhalo/video/service/imp/CategoryServiceImp.java
+++ src/main/java/net/xhalo/video/service/imp/CategoryServiceImp.java
@@ -4,6 +4,7 @@ import net.xhalo.video.dao.CategoryDao;
 import net.xhalo.video.model.Category;
 import net.xhalo.video.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,13 +13,16 @@ import java.util.List;
 @Service
 @Transactional
 public class CategoryServiceImp implements ICategoryService {
+
 	@Autowired
 	private CategoryDao categoryDao;
 
+	@Cacheable(value = "category", key = "#root.method")
 	public List<Category> getAllCategories() {
 		return categoryDao.getAllCategories();
 	}
 
+	@Cacheable(value = "category", key = "#obj.id")
 	public Category getCategoryById(Category obj) {
 		return categoryDao.getCategoryById(obj.getId());
 	}
