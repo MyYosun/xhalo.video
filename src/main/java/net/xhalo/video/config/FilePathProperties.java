17575271b43e634903b029ec4d5fd668b757b703
Yosun <602704853@qq.com>
2018-03-14 16:31:46 +0800

    使用properties文件加载路径

--- src/main/java/net/xhalo/video/config/FilePathProperties.java
+++ src/main/java/net/xhalo/video/config/FilePathProperties.java
@@ -1,16 +1,46 @@
 package net.xhalo.video.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class FilePathProperties {
-    //dev环境
-    public static final String VIDEO_SAVE_PATH = "D:\\media\\video\\";
-    public static final String IMAGE_SAVE_PATH = "D:\\media\\image\\";
-    public static final String HEAD_IMAGE_SAVE_PATH = "D:\\media\\image\\head\\";
-    public static final String BIG_IMAGE_SAVE_PATH = "D:\\media\\image\\big\\";
-    public static final String FFMPEG_PATH = "D:\\Develop Environment\\ffmpeg\\bin\\ffmpeg.exe";
-    //produce环境
-/*    public static final String VIDEO_SAVE_PATH = "/opt/media/video/";
-    public static final String IMAGE_SAVE_PATH = "/opt/media/image/";
-    public static final String HEAD_IMAGE_SAVE_PATH = "/opt/media/image/head/";
-    public static final String BIG_IMAGE_SAVE_PATH = "/opt/media/image/big/";
-    public static final String FFMPEG_PATH = "/usr/local/ffmpeg/bin/ffmpeg";*/
+
+    private static Logger logger = LoggerFactory.getLogger(FilePathProperties.class);
+
+    static {
+        loadProperties();
+    }
+
+    public static String VIDEO_SAVE_PATH;
+    public static String IMAGE_SAVE_PATH;
+    public static String HEAD_IMAGE_SAVE_PATH;
+    public static String BIG_IMAGE_SAVE_PATH;
+    public static String FFMPEG_PATH;
+
+    synchronized private static void loadProperties() {
+        Properties properties = new Properties();
+        InputStream in = FilePathProperties.class.getResourceAsStream("/config/properties/path.properties");
+        try {
+            properties.load(in);
+            VIDEO_SAVE_PATH = properties.getProperty("VIDEO_SAVE_PATH");
+            IMAGE_SAVE_PATH = properties.getProperty("IMAGE_SAVE_PATH");
+            HEAD_IMAGE_SAVE_PATH = properties.getProperty("HEAD_IMAGE_SAVE_PATH");
+            BIG_IMAGE_SAVE_PATH = properties.getProperty("BIG_IMAGE_SAVE_PATH");
+            FFMPEG_PATH = properties.getProperty("FFMPEG_PATH");
+        } catch (IOException e) {
+            logger.error("ERROR WHEN LOAD PROPERTY:", e);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                logger.error("ERROR WHEN CLOSE INPUT STREAM:", e);
+
+            }
+        }
+    }
+
 }
