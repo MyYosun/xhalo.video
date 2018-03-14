849963d5a3f46fdaef03d6c48196b99fb6f6c659
Yosun <602704853@qq.com>
2018-03-14 13:55:37 +0800

    改为war打包，修改名称，添加正式环境

--- src/main/java/net/xhalo/video/config/FilePathProperties.java
+++ src/main/java/net/xhalo/video/config/FilePathProperties.java
@@ -1,9 +1,16 @@
 package net.xhalo.video.config;
 
 public class FilePathProperties {
+    //dev环境
     public static final String VIDEO_SAVE_PATH = "D:\\media\\video\\";
     public static final String IMAGE_SAVE_PATH = "D:\\media\\image\\";
     public static final String HEAD_IMAGE_SAVE_PATH = "D:\\media\\image\\head\\";
     public static final String BIG_IMAGE_SAVE_PATH = "D:\\media\\image\\big\\";
     public static final String FFMPEG_PATH = "D:\\Develop Environment\\ffmpeg\\bin\\ffmpeg.exe";
+    //produce环境
+/*    public static final String VIDEO_SAVE_PATH = "/opt/media/video/";
+    public static final String IMAGE_SAVE_PATH = "/opt/media/image/";
+    public static final String HEAD_IMAGE_SAVE_PATH = "/opt/media/image/head/";
+    public static final String BIG_IMAGE_SAVE_PATH = "/opt/media/image/big/";
+    public static final String FFMPEG_PATH = "/usr/local/ffmpeg/bin/ffmpeg";*/
 }
